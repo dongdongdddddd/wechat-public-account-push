4bbde6881a98fb2d938ea2cd5103467491cd0343
dongdongdddddd <114078437+dongdongdddddd@users.noreply.github.com>
2022-09-21 21:06:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,72 +1,14 @@
 export const config = {
-  /**
-   * 公众号配置
-   */
-
+  //公众号配置
   // 公众号APP_ID
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_ID: "wx669a6cf1f932c979",
-
   // 公众号APP_SECRET
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: "a26568d7e0e4939146ef3feffeceff11",
-
-  // 是否给文字设置多彩颜色, 和emoji不兼容
-  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: true,
-
-  // 功能开关,打开：true，关闭：false
-  SWITCH: {
-    /** 每日天气 */
-    weather: true,
-
-    /** 节假日 */
-    // 下一休息日综合提醒
-    holidaytts: true,
-
-    /** 每日N句 */
-    // 金山每日一句
-    CIBA: true,
-    // 每日一言
-    oneTalk: false,
-    // 土味情话(彩虹屁)
-    earthyLoveWords: false,
-    // 朋友圈文案
-    momentCopyrighting: false,
-    // 毒鸡汤
-    poisonChickenSoup: false,
-    // 古诗古文
-    poetry: false,
-
-    /** 星座运势 */
-    horoscope: false,
-
-    /** 生日消息和节日消息 */
-    birthdayMessage: true,
-  },
-
-  /** 每日一言 */
-
-  // 每日一言的内容类型
-  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: "",
-  
-
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
-   */
-  USERS: [jiajiaxnadd]
-    {
-      // 想要发送的人的名字
-      name: "老婆0",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "9dnJjLdtHGz7GAm6i-2xxhYf7nE3A47heIZZSKk5D5w",
-      // 所在省份
-      province: "重庆",
+  //接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
+ user: [jiajiaxnadd]
+   //信息配置
+   // 所在省份
+    province: "重庆",
       // 所在城市
       city: "垫江",
       // 新历生日, 仅用作获取星座运势, 格式必须
@@ -78,4 +20,4 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "07-16"}
+        {"type": "*生日", "name": "老婆", "year": "1996", "date": "07-16"
